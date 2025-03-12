91fa2e203c687d1e697ded018f10d1b84d58825d
victorg-dev <vic.gheorghe@gmail.com>
2025-03-12 14:18:14 +0900

    update main.py with count_words function

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ path_to_frankenstein = "./books/frankenstein.txt"
 def get_book_text(path_to_file):
     print(f"Opening file: {path_to_file}")
     with open(path_to_file) as f:
-        file_contents = f.read()[:500] # TODO: remove [:500]
+        file_contents = f.read()
 
     return file_contents
 
+
+# Takes book contents as a string and returns the number of words
+def count_words(contents):
+    words_list = contents.split() # split all whitespace
+    return len(words_list)
+
+
 def main():
-    # store frankenstein contents
     frankenstein_contents = get_book_text(path_to_frankenstein)
+    frankenstein_word_count = count_words(frankenstein_contents)
 
-    print(f"CONTENTS:\n{frankenstein_contents}")
+    print(f"{frankenstein_word_count} words found in the document")
 
-main() # call main
+
+main()
