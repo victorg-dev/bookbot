1dd8032e6e7a1216473d30255c000f1f89a56939
victorg-dev <vic.gheorghe@gmail.com>
2025-03-13 13:06:58 +0900

    add argument validation to main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+import sys
 from stats import count_words
 from stats import count_characters
 from stats import sort_character_counts
 
-path_to_frankenstein = "books/frankenstein.txt"
-
 
 # Opens a file and returns the entire contents
 def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
+    with open(path_to_file) as file:
+        file_contents = file.read()
 
     return file_contents
 
@@ -28,14 +27,23 @@ def generate_report(path_to_book, book_word_count, book_sorted_character_counts)
     return report
 
 
+# The main function handles argument validation displaying
+# the book report to the user
 def main():
-    frankenstein_contents = get_book_text(path_to_frankenstein)
-    frankenstein_word_count = count_words(frankenstein_contents)
-    frankenstein_character_counts = count_characters(frankenstein_contents)
-    frankenstein_sorted_character_counts = sort_character_counts(frankenstein_character_counts)
-
-    frankenstein_report = generate_report(path_to_frankenstein, frankenstein_word_count, frankenstein_sorted_character_counts)
-    print(frankenstein_report)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    try:
+        book_contents = get_book_text(sys.argv[1])
+    except Exception as error: #filenotfound error
+        print(f"{error}\nEnter a correct file path.")
+        sys.exit(1)
+    
+    book_word_count = count_words(book_contents)
+    book_character_counts = count_characters(book_contents)
+    book_sorted_character_counts = sort_character_counts(book_character_counts)
+    book_report = generate_report(sys.argv[1], book_word_count, book_sorted_character_counts)
+    print(book_report)
 
 
 main()
